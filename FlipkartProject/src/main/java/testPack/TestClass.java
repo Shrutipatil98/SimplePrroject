fc655f6be25c58ac43407f1c847b08537b1977b2
Shrutipatil98 <147808034+Shrutipatil98@users.noreply.github.com>
2023-10-15 21:09:31 +0530

    delete the hello statement

--- FlipkartProject/src/main/java/testPack/TestClass.java
+++ FlipkartProject/src/main/java/testPack/TestClass.java
@@ -4,7 +4,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("hiii");
 		System.out.println("-------");
-		System.out.println("hello");
 
 		System.out.println("hiii");
 		System.out.println("-------");
