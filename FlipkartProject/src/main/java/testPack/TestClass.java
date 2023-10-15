1334d58946c42ad4cd4eb3aed9b66e90e8bdc8f5
DELL <DELL@DESKTOP-R9NNO27>
2023-10-15 20:50:44 +0530

    First commit

---
+++ FlipkartProject/src/main/java/testPack/TestClass.java
@@ -0,0 +1,10 @@
+package testPack;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("hiii");
+		System.out.println("-------");
+		System.out.println("hello");
+	}
+
+}
