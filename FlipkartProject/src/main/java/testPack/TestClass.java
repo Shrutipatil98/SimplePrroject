9adf3dc20a040f68d63633e82d0e2ae0e411c8c6
Shrutipatil98 <147808034+Shrutipatil98@users.noreply.github.com>
2023-10-15 23:34:02 +0530

    Add bye state

--- FlipkartProject/src/main/java/testPack/TestClass.java
+++ FlipkartProject/src/main/java/testPack/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("hiii");
 		System.out.println("-------");
-		System.out.println("hello");
+		System.out.println("Bye");
 	}
 
 }
